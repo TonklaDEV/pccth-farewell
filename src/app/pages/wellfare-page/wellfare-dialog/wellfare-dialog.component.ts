6a0963d0ae9390e066925c40086081fc626a5e78
NatnareeD <natnareed@pccth.com>
2024-02-05 16:19:39 +0700

    wellfare selectedRange

--- src/app/pages/wellfare-page/wellfare-dialog/wellfare-dialog.component.ts
+++ src/app/pages/wellfare-page/wellfare-dialog/wellfare-dialog.component.ts
@@ -32,30 +32,53 @@ export class WellfareDialogComponent implements OnInit {
       details: [null, Validators.required],
     });
   }
-  
+
   get dateRange() {
     return this.expenseFrom.get('dateRange') as FormGroup;
   }
 
+  get numberOfDays() {
+    return this.expenseFrom.get('numberOfDays') as FormGroup;
+  }
+
   updateNumberOfDays(): void {
-    const startDate = this.dateRange.get('start')?.value;
-    const endDate = this.dateRange.get('end')?.value;
+    console.log('Updating number of days...');
+    const dateRangeFormGroup = this.expenseFrom.get('dateRange');
+    const startDate = dateRangeFormGroup?.get('start')?.value;
+    const endDate = dateRangeFormGroup?.get('end')?.value;
+
+    console.log('Start Date:', startDate);
+    console.log('End Date:', endDate);
 
     if (startDate && endDate) {
-      const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        const startTimestamp = new Date(startDate).getTime();
+        const endTimestamp = new Date(endDate).getTime();
 
-      this.expenseFrom.get('numberOfDays')?.setValue(diffDays);
-      console.log(diffDays);
+        const timeDiff = Math.abs(endTimestamp - startTimestamp);
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        console.log('Calculated Diff Days:', diffDays);
+
+        const diffDaysString = diffDays.toString();
+        this.expenseFrom.get('numberOfDays')?.setValue(diffDaysString);
+        console.log('Updated numberOfDays:', diffDaysString);
+
+        // Convert timestamps to ISO strings in UTC timezone
+        const selectedRange = {
+            start: new Date(startTimestamp).toISOString(),
+            end: new Date(endTimestamp).toISOString(),
+        };
+        console.log('Selected range:', selectedRange);
     }
-  }
-  
+}
 
-  save(){
+
+
+  save() {
     console.log(this.expenseFrom);
   }
 
   ngOnInit(): void {
-
+  this.updateNumberOfDays();
   }
+  
 }
